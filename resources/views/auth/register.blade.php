c4c075fffa962114bb8ad717d85c3ca821beedb5
Michael J. Torres T <117607708+mjtorrest01@users.noreply.github.com>
2023-12-06 13:50:24 -0500

    Creando pagina de Registro
    se agregaron los formulario y una imagen representativa tipo SVG para darle un poco mas de estilo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,3 +3,83 @@
 @section('titulo')
     Registrate en DevStagram
 @endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center">
+        <div class="md:w-5/12">
+            <img src="{{ asset('img/Register.svg') }}" alt="">
+        </div>
+
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form>
+                <div class=" mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input 
+                        id="name"
+                        name="name"
+                        type="text"
+                        placeholder="Tu Nombre"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Username
+                    </label>
+                    <input 
+                        id="username"
+                        name="username"
+                        type="text"
+                        placeholder="Tu Username"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        E-mail
+                    </label>
+                    <input 
+                        id="email"
+                        name="email"
+                        type="email"
+                        placeholder="Tu Email de Registros"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Password
+                    </label>
+                    <input 
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Password de Registros"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Repetir Password
+                    </label>
+                    <input 
+                        id="password_confirmation"
+                        name="password_confirmation"
+                        type="password"
+                        placeholder="Repite tu Password"
+                        class="border p-3 w-full rounded-lg"
+                    />
+                </div>
+
+                <input 
+                    type="submit"
+                    value="Crear Cuenta"
+                    class=" bg-sky-600 hover:bg-sky-700 transition-colors
+                    cursor-pointer uppercase font-bold p-3 w-full text-white rounded-lg"
+                >
+            </form>
+        </div>
+    </div>
+@endsection
