af3747999b561363ec501c32220b22d6eb9418bb
Michael J. Torres T <117607708+mjtorrest01@users.noreply.github.com>
2023-12-06 10:55:17 -0500

    Creando Base
    Creando base de la vista del main y footer de la aplicación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,13 +21,16 @@
             </div>
         </header>
 
-        <main>
-
+        <main class=" container mx-auto mt-10">
+            <h2 class=" font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
         </main>
 
-        <footer>
-
+        <footer class=" text-center p-5 text-gray-500 font-bold uppercase">
+            DevStagram - Todos los derechos reservados {{ now()->year }}
         </footer>
-        
+
     </body>
 </html>
