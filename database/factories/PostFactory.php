b2e158e3f77175447819253a179cccc7e590793a
Michael J. Torres T <117607708+mjtorrest01@users.noreply.github.com>
2023-12-19 17:14:34 -0500

    Factory para relizar tsting de la base de datos
    sail artisan tinker
    
    App\Models\Post::factory()->times(200)->create();

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,10 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'titulo'=>$this->faker->sentence(5),
+            'descripcion' =>$this->faker->sentence(20),
+            'imagen'=>$this->faker->uuid() . '.jpg',
+            'user_id'=> $this->faker->randomElement([2, 3, 4, 5, 6])
         ];
     }
 }
